55c15d9e28cb026f70d5428d3038a0ba06f1e7b8
caitlin <cyl11603@gmail.com>
2021-10-13 00:00:13 -0400

    Made cases for intersection detected or not. Error: stack on kept_lines

--- mouse_code/Caitlin/OpenCV/pathPhoto.py
+++ mouse_code/Caitlin/OpenCV/pathPhoto.py
@@ -3,9 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import math
+from PIL import Image
 
 # Regular Houghlines with center line detection
-# TODO: identify intersection
+# TODO: line 90
 
 img = cv2.imread('cross_path1.jpg')
 img = img[int(len(img)/2):len(img), :]
@@ -33,8 +34,7 @@ maxSlope = 0
 minSlope_ind = 0
 minSlope = 0
 
-zeroSlope = 100
-zeroSlope_ind = 0
+n = 0       # n = number of horizontal lines
 
 # Find most pos and most neg slopes
 # Find slope closest to zero
@@ -50,33 +50,57 @@ for line in lines:
         elif slope<minSlope:
             minSlope = slope
             minSlope_ind = i
-        elif abs(slope)<abs(zeroSlope):
-            zeroSlope = slope
-            zeroSlope_ind = i
-        i = i+1
+        if abs(slope) < 0.1:
+            n += 1
+        i += 1
 
+intLines = 0    # =0 if no intersecting lines
 
-# Find second closest to zero slope, wtih some distance between original and identified lines
-zeroSlope2 = 100
-zeroSlope2_ind = 0
-i = 0
-for line in lines:
-    for rho,theta in line:
-        if i != zeroSlope_ind:
+if n > 1:       # there are at least 2 potential intersection lines
+    print("There are intersecting lines")
+
+    zeroSlope = 100
+    zeroSlope_ind = -1
+    i = 0
+    for line in lines:
+        for rho,theta in line:
             slope =  -1/math.tan(theta)
-            if abs(slope)<abs(zeroSlope2) and rho-lines[zeroSlope_ind][0][0]>3:
-                zeroSlope2 = slope
-                zeroSlope2_ind = i
-        i = i+1
 
-print("zeroSlope: ", zeroSlope, ", zeroSlope2: ", zeroSlope2)
+            if abs(slope)<abs(zeroSlope):
+                zeroSlope = slope
+                zeroSlope_ind = i
+            i += 1
+
+    zeroSlope2 = 1
+    zeroSlope2_ind = zeroSlope_ind
+    i = 0
+    for line in lines:
+        for rho,theta in line:
+            if i != zeroSlope_ind:
+                slope =  -1/math.tan(theta)
+                if abs(slope)<abs(zeroSlope2) and rho-lines[zeroSlope_ind][0][0]>3:
+                    zeroSlope2 = slope
+                    zeroSlope2_ind = i
+                    intLines = 1    # =1 if intersecting lines present
+            i += 1
+
+    print("zeroSlope: ", zeroSlope, " @ i=", zeroSlope_ind, ", zeroSlope2: ", zeroSlope2, " @ i=", zeroSlope2_ind)
 
 # Make new array with only steepest and horizontal-est edges
-kept_lines = np.stack((lines[maxSlope_ind], lines[minSlope_ind], lines[zeroSlope_ind], lines[zeroSlope2_ind]))
-slopes = [maxSlope, minSlope, zeroSlope, zeroSlope2]
-print("slopes: ")
-print(slopes)
-print()
+# TODO: debug np.stack; need to extend dimension to add zeroSlope and zeroSlope2
+slopes = [maxSlope, minSlope]
+kept_lines = np.stack((lines[maxSlope_ind], lines[minSlope_ind]))
+if intLines == 1:
+    print("appending horizontal lines")
+    kept_lines = np.stack((lines[zeroSlope_ind], lines[zeroSlope2_ind]))
+    slopes.append(zeroSlope)
+    slopes.append(zeroSlope2)
+    print("slopes: ")
+    print(slopes)
+    print()
+
+print("kept_lines: ")
+print(kept_lines)
 
 # derive vertical centerline
 der_coords = []
@@ -115,32 +139,33 @@ print("(", c_line_x1, ", ", c_line_y1, "), (", c_line_x2, ", ", c_line_y2, ")")
 cv2.line(img,(c_line_x1,c_line_y1),(c_line_x2,c_line_y2),(255,0,0),2)
 
 # Find intersections
-def intersection(l1, l2):
-    rho1, theta1 = l1[0]
-    rho2, theta2 = l2[0]
-    A = np.array([
-        [np.cos(theta1), np.sin(theta1)],
-        [np.cos(theta2), np.sin(theta2)]
-    ])
-    b = np.array([[rho1], [rho2]])
-    x0, y0 = np.linalg.solve(A, b)
-    x0, y0 = int(np.round(x0)), int(np.round(y0))
-    return [[x0, y0]]
+if intLines == 1:
+    def intersection(l1, l2):
+        rho1, theta1 = l1[0]
+        rho2, theta2 = l2[0]
+        A = np.array([
+            [np.cos(theta1), np.sin(theta1)],
+            [np.cos(theta2), np.sin(theta2)]
+        ])
+        b = np.array([[rho1], [rho2]])
+        x0, y0 = np.linalg.solve(A, b)
+        x0, y0 = int(np.round(x0)), int(np.round(y0))
+        return [[x0, y0]]
 
-intersects = []
-i = 0
-while i<2:
-    j = 2
-    while j<4:
-        intersects.append(intersection(kept_lines[i], kept_lines[j]))
-        j = j+1
-    i = i+1
+    intersects = []
+    i = 0
+    while i<2:
+        j = 2
+        while j<4:
+            intersects.append(intersection(kept_lines[i], kept_lines[j]))
+            j = j+1
+        i = i+1
 
-print(intersects)
+    print(intersects)
 
-for point in intersects:
-    for x,y in point:
-        cv2.circle(img,(x,y),radius=1,color=(0,0,255),thickness=2)
+    for point in intersects:
+        for x,y in point:
+            cv2.circle(img,(x,y),radius=1,color=(0,0,255),thickness=2)
 
 cv2.imwrite('cross_path_c_line.jpg',img)
 im = Image.open('cross_path_c_line.jpg')
