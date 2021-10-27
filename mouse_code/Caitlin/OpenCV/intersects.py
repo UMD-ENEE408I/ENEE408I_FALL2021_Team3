5c15ab09e1fbe9133891b9d6059235fa7e2cfa7a
caitlin <cyl11603@gmail.com>
2021-10-27 14:53:33 -0400

    Added comments

--- mouse_code/Caitlin/OpenCV/intersects.py
+++ mouse_code/Caitlin/OpenCV/intersects.py
@@ -7,16 +7,16 @@ from PIL import Image
 
 # Regular Houghlines with center line detection
 
-img = cv2.imread('33mm_calib.jpg')
-img = img[int(len(img)/2):len(img), 10:len(img[0])-10]
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,200,255)
-cv2.imwrite('edges.jpg',edges)
-lines = cv2.HoughLines(edges,1,np.pi/180,50)
+img = cv2.imread('33mm_calib.jpg')                          # read image
+img = img[int(len(img)/2):len(img), 10:len(img[0])-10]      # crop image
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)                 # convert to grayscale
+edges = cv2.Canny(gray,200,255)                             # edge detection with Canny
+cv2.imwrite('edges.jpg',edges)                              # write edges to image
+lines = cv2.HoughLines(edges,1,np.pi/180,50)                # detect lines
 print("All lines: ")
 print(lines)
 
-
+# Calculate coordinates for all detected lines
 for line in lines:
     for rho,theta in line:
         a = np.cos(theta)
@@ -29,7 +29,7 @@ for line in lines:
         y2 = int(y0 - 1000*(a))
         #cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
-# find most vertical edges (main path edges) and most horizontal edges (intersection edges)
+# Begin selecting path edges
 i = 0
 maxSlope_ind = 0
 maxSlope = 0
@@ -58,9 +58,9 @@ for line in lines:
                 n += 1
         i += 1
 
-intLines = 0    # =0 if no intersecting lines
+intLines = 0    # intLines=0 if no intersecting lines
 
-if n > 1:       # there are at least 2 potential intersection lines
+if n > 1:       # There are at least 2 potential intersection lines
     print("There are intersecting lines")
 
     zeroSlope = 100
@@ -82,7 +82,7 @@ if n > 1:       # there are at least 2 potential intersection lines
     for line in lines:
         for rho,theta in line:
             if i != zeroSlope_ind:
-                if theta != 0:
+                if theta != 0:          # Ignore perfectly vertical lines
                     slope =  -1/math.tan(theta)
                     print("zeroSlope2 = ", zeroSlope2, "\tslope = ", slope)
                     print("\trho = ", rho, "\tzeroSlope rho = ", lines[zeroSlope_ind][0][0])
@@ -90,18 +90,21 @@ if n > 1:       # there are at least 2 potential intersection lines
                         print("\tintLines=1")
                         zeroSlope2 = slope
                         zeroSlope2_ind = i
-                        intLines = 1    # =1 if intersecting lines present
+                        intLines = 1    # intLines=1 if intersecting lines present
             i += 1
 
     print("zeroSlope: ", zeroSlope, " @ i=", zeroSlope_ind, ", zeroSlope2: ", zeroSlope2, " @ i=", zeroSlope2_ind)
 
-# Make new array with only steepest and horizontal-est edges
+# Make new array with only main path and intersection path edges
+# Initialize array to main path edges
 slopes = [maxSlope, minSlope]
 kept_lines = np.stack((lines[maxSlope_ind], lines[minSlope_ind]))
 print("kept_lines before adding intLines: ")
 print(kept_lines)
 print("\tsize: ", kept_lines.shape)
 print()
+
+# Append intersection path edges if present
 if intLines == 1:
     print("appending horizontal lines")
     kept_lines = np.concatenate((kept_lines, [lines[zeroSlope_ind]]), axis=0)
@@ -115,7 +118,7 @@ if intLines == 1:
     print()
 
 
-# derive vertical centerline
+# Derive vertical centerline from main path edges
 der_coords = []
 
 i = 0
@@ -151,8 +154,8 @@ print("c_line: ")
 print("(", c_line_x1, ", ", c_line_y1, "), (", c_line_x2, ", ", c_line_y2, ")")
 cv2.line(img,(c_line_x1,c_line_y1),(c_line_x2,c_line_y2),(255,0,0),2)
 
-# Find intersections
-if intLines == 1:
+# Find intersection points if intersecting path is present
+if intLines == 1:       # There are intersections
     def intersection(l1, l2):
         rho1, theta1 = l1[0]
         rho2, theta2 = l2[0]
