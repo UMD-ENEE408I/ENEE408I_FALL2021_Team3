d00dbb6eca67879dbe1389a1bc2f12b548409b3c
dlee021 <dlee12321@yahoo.com>
2021-12-03 00:54:18 -0500

    editted client to match changes to the new server functions

--- mouse_code/Daniel/Server stuff/Client/client.py
+++ mouse_code/Daniel/Server stuff/Client/client.py
@@ -7,21 +7,22 @@ import json
 load_dotenv()
 ROBOT_ID = os.getenv("ROBOT_ID")
 LINK = os.getenv("LINK")
+START_X = os.getenv("START_X")
+START_Y = os.getenv("START_Y")
+START_DIR = os.getenv("START_DIR")
 
-def sendCoords(x, y, robotID):
-    x = requests.post(LINK + "/coords/" + str(robotID), data={"x": x, "y": y})
-    print(x)
+def resetServer():
+    x = requests.post(LINK + "/resetServer", data={"code": "codesonooopsies"})
+    print(x.json())
 
-def getData():
-    ret = requests.get(LINK + "/dumpdata")
+def startMaze():
+    x = requests.post(LINK + "/start/" + str(ROBOT_ID), data={"x": START_X, "y": START_Y, "dir": START_DIR})
+    print(x.json()) # should be direction command
 
-    data = ret.json()
+    # send direction command to Jetson
 
-def clearData():
-    ret = requests.get(LINK + "/clear")
+def sendCoords(nodex, nodey, nodeType):
+    x = requests.post(LINK + "/coords/" + str(ROBOT_ID), data={"nodex": nodex, "nodey": nodey, "type": nodeType})
+    print(x.json()) # should be direction command
 
-    data = ret.json()
-
-sendCoords(10,20,1)
-
-getData()
+    # send direction command to Jetson
