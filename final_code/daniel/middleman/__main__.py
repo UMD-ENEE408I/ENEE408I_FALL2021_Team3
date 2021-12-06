c39e921718a190e6ff432173e9ea2febf911eea8
Oazubuko <89928629+Oazubuko@users.noreply.github.com>
2021-12-05 19:51:34 -0500

    updates to middleman python scripts

--- final_code/daniel/middleman/__main__.py
+++ final_code/daniel/middleman/__main__.py
@@ -7,49 +7,52 @@ import time
 if __name__ == '__main__':
     vid = cam.initialize()
 
-    rfJetson = RFJetson("/dev/cu.usbserial-1440")
+    rfJetson = RFJetson("COM11")
 
-    time.sleep(5)
+    time.sleep(2)
 
     resp = client.resetServer()
 
     resp = client.startMaze()
-
-
-    while resp is None:
-        resp = client.startMaze()
-        time.sleep(10)
     
-    time.sleep(3)
+    time.sleep(2)
 
-    mouseResp = rfJetson.send(resp)
+    mouseResp = rfJetson.send(resp, 15)
 
     if mouseResp == (200,200):
         print("mouse responded")
 
 
-    time.sleep(3)
+    time.sleep(1)
 
     while True:
         typeArr = []
+        typeArrDict = {}
         for i in range(0, 4):
             (intersectType, dist) = cam.intersect(vid)
             typeArr.append(intersectType)
-            print(intersectType)
+            if intersectType in typeArrDict.keys():
+                newdist = (typeArrDict[intersectType] + dist) / 2
+                typeArrDict[intersectType] = newdist
+            else:
+                typeArrDict[intersectType] = dist
+            print(intersectType, " ", dist)
 
         intersectType = max(set(typeArr), key=typeArr.count)
+        intersectDist = typeArrDict[intersectType] if intersectType != 0 else 15
+        intersectDist = int(intersectDist)
         print("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
-        print(intersectType)
+        print(intersectType, " ", dist)
 
         resp = client.sendCoords(intersectType)
 
         if resp is not None:
             if resp == 2 or resp == 3: #left or right
-                mouseResp = rfJetson.send(1)
-                time.sleep(1)
+                mouseResp = rfJetson.send(1, intersectDist)
+                time.sleep(2)
                 if mouseResp != (200, 200):
                     break
-            mouseResp = rfJetson.send(resp)
+            mouseResp = rfJetson.send(resp, intersectDist)
             time.sleep(1)
             if mouseResp == (200,200):
                 print("mouse responded")
